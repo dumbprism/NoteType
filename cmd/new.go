07bdf497c14f35e2ddf735da15fb2e243204a8d7
Dumbprism <arhantk915@gmail.com>
2025-07-22 20:21:07 +0530

    made some changes with the new command

--- cmd/new.go
+++ cmd/new.go
@@ -1,67 +1,53 @@
-/*
-Copyright © 2025 NAME HERE <EMAIL ADDRESS>
-*/
 package cmd
 
 import (
 	"fmt"
 	"os"
-	"path/filepath"
 	"time"
 
 	"github.com/spf13/cobra"
 )
 
-var pageDetails struct {
-	dateTime string
-}
 
-// File Creation -> The file format must be markdown
-func createAndWriteFile(filename string, title string) {
-	// creation part
-	file, err := os.Create(filename)
+func createAndAddFile(filename string, title string, entry string) {
+
+	file, err := os.Create(filename + ".md")
 	if err != nil {
 		fmt.Println(err)
 	}
 	defer file.Close()
-	fmt.Println("file " + filename + " was created succesfully.")
-	fmt.Println(filepath.Ext(filename))
 
-	// writing part -> for now it gives a generic template of file creation.
+	// writing inside the file
+	var currentDate = time.Now().String()[0:10]
+	fmt.Println()
+	var style_open = `<span style="opacity:0.5">`
+	var style_close = "</span>"
+	var structure = "# " + title + "\n" + style_open + currentDate + style_close + "\n" + "---"
 
-	pageDetails.dateTime = time.Now().String()[0:10]
+	file.WriteString(structure + "\n" + entry)
 
-	var opacity = "opacity:0.5"
-	var style_opening = "<span style=" + opacity+ ">"
-	var style_closing = "</span>"
-	
-	_, err = file.WriteString("# " + title + "\n" + style_opening + pageDetails.dateTime + style_closing + "\n" + "---")
-
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("File written sucessfully")
-	}
+	fmt.Println("file " + filename + " has been successfully created.")
 }
 
 // newCmd represents the new command
 var newCmd = &cobra.Command{
 	Use:   "new",
-	Short: "If you are willing to add a new entry, this is your starting point",
+	Short: "helps you to add new entry to your daily journal",
+	Args:  cobra.ExactArgs(3),
 	Long: `
-		The new command helps you to make new entries and create new notes
-		to get you starting and brainstorm all your ideas
+		This is where you start typing your thoughts and other things that you wish to type down.
+		Don't stop and let your thoughts flow. 
 	`,
-	Args: cobra.ExactArgs(2),
 	Run: func(cmd *cobra.Command, args []string) {
 		var filename = args[0]
 		var title = args[1]
-		createAndWriteFile(filename, title)
+		var entry = args[2]
+
+		createAndAddFile(filename, title, entry)
 	},
 }
 
 func init() {
 	rootCmd.AddCommand(newCmd)
 
-	// no flags as of now
 }
---
+++ test.md
@@ -0,0 +1,4 @@
+# Test
+<span style="opacity:0.5">2025-07-22</span>
+---
+this is me testing out my CLI application using cobra CLI \n bbut it is quite boring to do so though. LOL
