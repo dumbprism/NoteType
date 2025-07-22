2a4dda3eaf0b0470e1e27fcb6b752fa460ab21d1
Dumbprism <arhantk915@gmail.com>
2025-07-22 21:02:24 +0530

    added the update command

---
+++ cmd/update.go
@@ -0,0 +1,46 @@
+package cmd
+
+import (
+	"fmt"
+	"os"
+	
+	"github.com/spf13/cobra"
+)
+
+// function to update files
+
+func updateFile(filename string,content string){
+	// appending to the file
+	file,err := os.OpenFile(filename+".md",os.O_APPEND,6660)
+	
+	if err != nil{
+		fmt.Println(err)
+	}
+
+	if _,err := file.Write([]byte("\n"+content));err != nil{
+		file.Close()
+		fmt.Println(err)
+	}
+
+
+}
+
+// updateCmd represents the update command
+var updateCmd = &cobra.Command{
+	Use:   "update",
+	Short: "Appends data into an existing file",
+	Long: `Helps you to update your file if you have missed something or would like to add something
+	so that you don't miss out on anything
+	`,
+	Args: cobra.ExactArgs(2),
+	Run: func(cmd *cobra.Command, args []string) {
+		var filename = args[0]
+		var content = args[1]
+
+		updateFile(filename,content)
+	},
+}
+
+func init() {
+	rootCmd.AddCommand(updateCmd)
+}
---
+++ hello.md
@@ -0,0 +1,5 @@
+# Test
+<span style="opacity:0.5">2025-07-22</span>
+---
+this is a random test text
+Updated content:  I almost forgot to add this point to my file
